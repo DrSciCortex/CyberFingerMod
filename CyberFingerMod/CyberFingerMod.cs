a8c49336faca026e9d50c710a0c720827cac4d6d
DrSciCortex <youremail@yourdomain.com>
2025-11-11 01:54:36 -0500

    Cleanup some cruft

--- CyberFingerMod/CyberFingerMod.cs
+++ CyberFingerMod/CyberFingerMod.cs
@@ -19,7 +19,7 @@ using static Elements.Core.Pool;
 namespace CyberFingerMod;
 //More info on creating mods can be found https://github.com/resonite-modding-group/ResoniteModLoader/wiki/Creating-Mods
 public class CyberFingerMod : ResoniteMod {
-	internal const string VERSION_CONSTANT = "1.0.1"; //Changing the version here updates it in all locations needed
+	internal const string VERSION_CONSTANT = "1.1.1"; //Changing the version here updates it in all locations needed
 	public override string Name => "CyberFingerMod";
 	public override string Author => "DrSciCortex";
 	public override string Version => VERSION_CONSTANT;
@@ -123,13 +123,17 @@ public class CyberFingerMod : ResoniteMod {
 		static private ScreenLocomotionDirection GenerateScreenDirection(StandardGamepad __instance) {
 			ScreenLocomotionDirection screenLocomotionDirection = new ScreenLocomotionDirection();
 			screenLocomotionDirection.Axis = InputNode.Analog2D(__instance.LeftThumbstick);
-			screenLocomotionDirection.Up = InputNode.Digital(__instance.RightThumbstickClick);
-			screenLocomotionDirection.Down = InputNode.Digital(__instance.LeftThumbstickClick);
+			screenLocomotionDirection.Up = InputNode.Digital(__instance.LeftThumbstickClick);
+			screenLocomotionDirection.Down = InputNode.Digital(__instance.RightThumbstickClick);
 			return screenLocomotionDirection;
 		}
 
 		static private VR_SingleLocomotionTurn GenerateTurn(StandardGamepad __instance) {
 			return new VR_SingleLocomotionTurn(InputNode.Analog2D(__instance.RightThumbstick).X());
+		}
+
+		static private VR_SingleLocomotionTurn GenerateTurnLeft(StandardGamepad __instance) {
+			return new VR_SingleLocomotionTurn(InputNode.Analog2D(__instance.LeftThumbstick).X());
 		}
 
 
@@ -215,14 +219,107 @@ public class CyberFingerMod : ResoniteMod {
 
 			}*/
 
-			// TODO VR_LocomotionDirection, LocomotionTurn
+			
+			if (group is SmoothThreeAxisLocomotionInputs threeAxisLocomotion) {
+				threeAxisLocomotion.Move.AddBinding(GenerateScreenDirection(__instance), __instance);
+				threeAxisLocomotion.Jump.AddBinding(__instance.LeftThumbstickClick);
+				threeAxisLocomotion.Align.AddBinding(__instance.B);
+				threeAxisLocomotion.Align.AddBinding(__instance.Y);
+
+				var rs = InputNode.Analog2D(__instance.RightThumbstick);
+				var pitch = rs.Y().Negate();  // invert so up = pitch up
+				var yaw = rs.X();
+
+				var right = InputNode.Digital(__instance.X).ToAnalog();
+				var left = InputNode.Digital(__instance.A).ToAnalog();
+
+				// 2) Negate the "left" (either via .Negate() or multiply by -1)
+				var leftNeg = left.Negate();
+
+				// 3) Sum: right + (-left)
+				var rollSum = new SumInputs<float>();
+				rollSum.Inputs.Add(right);
+				rollSum.Inputs.Add(leftNeg);
+
+				var turn = InputNode.XYZ(pitch, yaw, rollSum);
+				threeAxisLocomotion.TurnDelta.AddBinding(turn, __instance);
+			}
+
+			if (group is AnchorLocomotionInputs anchorInputs) {
+
+				anchorInputs.PrimaryAxis.AddBinding(__instance.LeftThumbstick);
+				anchorInputs.SecondaryAxis.AddBinding(__instance.RightThumbstick);
+				anchorInputs.PrimaryAction.AddBinding(__instance.LeftThumbstickClick);
+				anchorInputs.SecondaryAction.AddBinding(__instance.RightThumbstickClick);
+			}
+
+			if (group is AnchorReleaseInputs anchorRelease) {
+				anchorRelease.Release.AddBinding(__instance.A);
+				anchorRelease.Release.AddBinding(__instance.X);
+				anchorRelease.ReleaseStrength.AddBinding(InputNode.Analog2D(__instance.LeftThumbstick).Magnitude(), __instance);
+				anchorRelease.ReleaseStrength.AddBinding(InputNode.Analog2D(__instance.RightThumbstick).Magnitude(), __instance);
+			}
+
+			if (group is TeleportInputs teleport) {
+				if (teleport.Side == Chirality.Left) {
+					teleport.Teleport.AddBinding(__instance.LeftThumbstickClick);
+					teleport.Backstep.AddBinding(InputNode.Analog2D(__instance.LeftThumbstick).Y().Negate()
+						.ToDigital(0.8f), __instance);
+					teleport.TurnDelta.AddBinding(GenerateTurnLeft(__instance), __instance);
+				}
+
+				if (teleport.Side == Chirality.Right) {
+					teleport.Teleport.AddBinding(__instance.RightThumbstickClick);
+					teleport.Backstep.AddBinding(InputNode.Analog2D(__instance.RightThumbstick).Y().Negate()
+						.ToDigital(0.8f), __instance);
+					teleport.TurnDelta.AddBinding(GenerateTurn(__instance), __instance);
+				}
+
+			}
+
+			if (group is DevToolInputs devTool) {
+
+				if (devTool.Side == Chirality.Left) {
+					Msg("Added Left DevToolInputs");
+					devTool.Focus.AddBinding(__instance.LeftThumbstickClick);
+					devTool.Inspector.AddBinding(__instance.X);
+					devTool.Create.AddBinding(__instance.Y);
+				}
+
+				if (devTool.Side == Chirality.Right) {
+					Msg("Added Right DevToolInputs");
+					devTool.Focus.AddBinding(InputNode.Digital(__instance.RightThumbstickClick), __instance);
+					devTool.Inspector.AddBinding(InputNode.Digital(__instance.A), __instance);
+					devTool.Create.AddBinding(InputNode.Digital(__instance.B), __instance);
+				}
+
+			}
+
+			if (group is LaserHoldInputs laserHold) {
+				if (laserHold.Side == Chirality.Left) {
+					laserHold.Align.AddBinding(InputNode.Digital(__instance.LeftThumbstickClick), __instance);
+					laserHold.Slide.AddBinding(InputNode.Analog2D(__instance.LeftThumbstick).Y(), __instance);
+					laserHold.Rotate.AddBinding(InputNode.Analog2D(__instance.LeftThumbstick).X(), __instance);
+				}
+
+				if (laserHold.Side == Chirality.Right) {
+					laserHold.Align.AddBinding(InputNode.Digital(__instance.RightThumbstickClick), __instance);
+					laserHold.Slide.AddBinding(InputNode.Analog2D(__instance.RightThumbstick).Y(), __instance);
+					laserHold.Rotate.AddBinding(InputNode.Analog2D(__instance.RightThumbstick).X(), __instance);
+				}
+			}
+
+
+			if (group is HeadInputs headInputs) {
+				headInputs.Crouch.AddBinding(InputNode.Digital(__instance.RightThumbstickClick).ToAnalog(), __instance);
+				headInputs.Crouch.AddBinding(InputNode.Digital(__instance.RightThumbstickClick).MultiTapToggle().ToAnalog(), __instance);
+			}
 
 			if (group is SmoothLocomotionInputs smoothLocomotion) {
 				smoothLocomotion.Move.AddBinding(GenerateScreenDirection(__instance), __instance);
 				smoothLocomotion.Jump.AddBinding(__instance.LeftThumbstickClick);
 				smoothLocomotion.TurnDelta.AddBinding(GenerateTurn(__instance), __instance);
 			}
-
 
 			if (group is InteractionHandlerInputs commonTool) {
 
@@ -251,7 +348,7 @@ public class CyberFingerMod : ResoniteMod {
 			}
 
 
-				if (group is GlobalActions globalActions) {
+			if (group is GlobalActions globalActions) {
 				//globalActions.ToggleDash.AddBinding(__instance.Menu); // left
 				//globalActions.ToggleDash.AddBinding(__instance.Start); //right
 
@@ -273,122 +370,5 @@ public class CyberFingerMod : ResoniteMod {
 
 
 	}
-
-
-	private void StripStandardGamepadBindings(InteractionHandlerInputs hand) {
-		if (hand == null) return;
-		PurgeBool((InputAction<bool>)hand.Interact);
-		PurgeBool((InputAction<bool>)hand.Secondary);
-		PurgeBool((InputAction<bool>)hand.Grab);
-		PurgeBool((InputAction<bool>)hand.Menu);
-		PurgeBool((InputAction<bool>)hand.UserspaceToggle);
-		// PurgeFloat ((InputAction<float>)  hand.Strength);
-		// PurgeFloat2((InputAction<float2>) hand.Axis);
-	}
-
-	public static InteractionHandlerInputs GetHandInputs(User user, Chirality chirality) {
-		if (user?.Input == null) return null;
-
-		// Each hand’s interaction inputs are stored under the InteractionHandlerInputModule
-		var handler = user.GetInteractionHandler(chirality);
-		return handler?.Inputs;
-	}
-
-	private void SetupBindings() {
-		var world = Engine.Current.WorldManager.FocusedWorld;
-		var user = world?.LocalUser;
-		if (user == null) return;
-
-		// 1) Get the left/right InteractionHandlerInputs ("commonTool" in your snippet)
-		// Adjust accessors to your codebase; the idea is to get:
-		//   leftCommon:  InteractionHandlerInputs  for left hand
-		//   rightCommon: InteractionHandlerInputs  for right hand
-		//var leftCommon = user.Input.LeftHand?.Interaction;   // aka your "commonTool" for LEFT
-		//var rightCommon = user.Input.RightHand?.Interaction;  // aka your "commonTool" for RIGHT
-
-		var leftCommon = GetHandInputs(world.LocalUser, Chirality.Left);
-		var rightCommon = GetHandInputs(world.LocalUser, Chirality.Right);
-		
-		// 2) Fetch the built-in Gamepad device (so we reuse Resonite's deadzones & OS backends)
-		var ii = Engine.Current.InputInterface;
-		var gamepad = ii.GetDevice<StandardGamepad>();  // name may be IGamepadDevice / SDLGamepad / XInputGamepad in your build
-		if (gamepad == null) return;
-
-		// avoid double triggers
-		if (leftCommon != null) StripStandardGamepadBindings(leftCommon);
-		if (rightCommon != null) StripStandardGamepadBindings(rightCommon);
-
-
-		//
-		// We'll mirror that, but source the signals from the Gamepad device:
-		//rightCommon.UserspaceToggle.AddBinding(gamepad.ButtonA);       // A -> right hand userspace toggle
-		//rightCommon.Menu.AddBinding(gamepad.ButtonB);                  // B -> right hand menu
-		//rightCommon.Grab.AddBinding(gamepad.RightShoulder);            // RB -> right hand grab
-		//rightCommon.Secondary.AddBinding(gamepad.RightStickPress);     // RS -> right hand secondary
-		//rightCommon.Interact.AddBinding(gamepad.RightTriggerClick);    // (edge) if exposed
-		//rightCommon.Strength.AddBinding(gamepad.RightTrigger);         // RT analog -> Strength
-		//rightCommon.Axis.AddBinding(gamepad.RightStick);               // right stick axes -> Axis
-		rightCommon.UserspaceToggle.AddBinding(gamepad.Menu);
-		rightCommon.Interact.AddBinding(gamepad.A);
-		rightCommon.Grab.AddBinding(gamepad.B);
-		rightCommon.Menu.AddBinding(gamepad.RightBumper);
-		rightCommon.Secondary.AddBinding(gamepad.Start);
-
-
-		// 4) Bind LEFT-HAND actions to X/Y/LB/LS and left stick/trigger
-		//leftCommon.Interact.AddBinding(gamepad.ButtonX);               // X -> left hand interact (primary)
-		//leftCommon.Secondary.AddBinding(gamepad.ButtonY);              // Y -> left hand secondary
-		//leftCommon.Grab.AddBinding(gamepad.LeftShoulder);              // LB -> left hand grab
-		//leftCommon.Secondary.AddBinding(gamepad.LeftStickPress);       // LS -> treat as secondary/alt
-		//leftCommon.Strength.AddBinding(gamepad.LeftTrigger);           // LT analog -> Strength
-		//leftCommon.Axis.AddBinding(gamepad.LeftStick);                 // left stick axes -> Axis
-		//leftCommon.UserspaceToggle.AddBinding(gamepad.Menu);
-		leftCommon.Interact.AddBinding(gamepad.X);
-		leftCommon.Grab.AddBinding(gamepad.Y);
-		leftCommon.Menu.AddBinding(gamepad.LeftBumper);
-		//leftCommon.Secondary.AddBinding(gamepad.Start);
-
-		// TIP: If your Gamepad API doesn’t expose "RightTriggerClick", you can synthesize it:
-		//   rightCommon.Interact.AddBinding(new EdgeBool(gamepad.RightTrigger, threshold: 0.5f));
-		//
-		// And if your API uses different names, the mapping is straightforward:
-		//   ButtonA/B/X/Y, Left/RightShoulder, Left/RightStickPress, Left/RightTrigger, Left/RightStick (float2)
-	}
-
-	static void PurgeBool(InputAction<bool> action) => PurgeImpl(action);
-	static void PurgeFloat(InputAction<float> action) => PurgeImpl(action);
-	static void PurgeFloat2(InputAction<float2> action) => PurgeImpl(action);
-
-	static void PurgeImpl<T>(InputAction<T> action) where T : struct 
-	{
-		if (action == null) return;
-
-		var tAction = typeof(InputAction<T>);
-		var fBindings = tAction.GetField("_bindings", BindingFlags.Instance | BindingFlags.NonPublic);
-		if (fBindings == null) return;
-
-		var list = fBindings.GetValue(action) as IList; // List<InputBinding<T>> as non-generic IList
-		if (list == null) return;
-
-		// iterate backwards so RemoveAt is safe
-		for (int i = list.Count - 1; i >= 0; i--) {
-			var binding = list[i];
-			var dev = GetMember(binding, "Device") ?? GetMember(binding, "_device");
-			if (dev is StandardGamepad)
-				list.RemoveAt(i);
-		}
-
-	}
-
-	static object GetMember(object obj, string name) {
-		if (obj == null) return null;
-		var t = obj.GetType();
-		var p = t.GetProperty(name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-		if (p != null) return p.GetValue(obj);
-		var f = t.GetField(name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-		if (f != null) return f.GetValue(obj);
-		return null;
-	}
-
 
 }
